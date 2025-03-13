a6b9bcc23951d8c08fc96b9f97914d11997e38e9
kozadireza <hanna.zhamoida1993@gmail.com>
2025-03-13 16:35:33 +0000

    Task 16 was done. New End Point was added into the users-router. Model and controller files were created for it. Full TDD was done

--- routers/routers_/users-router.js
+++ routers/routers_/users-router.js
@@ -1,7 +1,12 @@
 const { getAllUsers } = require("../../controllers/getAllUsers.controller");
+const {
+  getUserBy_Name,
+} = require("../../controllers/getUserBy_Username.controller");
 
 const usersRouter = require("express").Router();
 
 usersRouter.route("/").get(getAllUsers);
+
+usersRouter.route("/:username").get(getUserBy_Name);
 
 module.exports = usersRouter;
